11900e04a414b324838be3758a5c4df744ec2b56
ElAlgoritmoDeLaNoche <elalgoritmodelanochebazinga@gmail.com>
2022-10-22 16:20:08 -0500

    Cambio de localhost a url

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
-const api = axios.create({ baseURL: "http://localhost:8000" })
-// const api = axios.create({ baseURL: "https://instaverse-back.vercel.app" })
+// const api = axios.create({ baseURL: "http://localhost:8000" })
+const api = axios.create({ baseURL: "https://server-dayapp.vercel.app" })
 
 api.interceptors.request.use((req) => {
 
