745e3df1fa71db9103ab7a1b1dd4cd3d925b3791
ElAlgoritmoDeLaNoche <elalgoritmodelanochebazinga@gmail.com>
2022-10-22 19:18:03 -0500

    Validación de usuarios

--- src/Components/Home/Note/Note.js
+++ src/Components/Home/Note/Note.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { deleteNote } from '../../../actions/notes'
 import { Button } from 'antd'
@@ -10,35 +10,75 @@ const Note = ({ setSelectedId, note }) => {
 
   const dispatch = useDispatch();
 
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')))
+
+  const id = user?.result?._id
+
   return (
-    <div className='grid-container'>
-      <div
-        className="grid-item"
-        style={{ background: `#${note.color}` }}
-        key={note._id}
-      >
-        <div className="content-text">
-          <Link to={`/detail-note/${note._id}/${note.title}/${note.description}/${note.tags}/${note.color}/${note.postDate}`}>
-            <div className="content-title">
-              <h4>{note.title}</h4>
-              <div className="right-tag" style={{ background: '#fff', color: `#${note.color}` }}>{note.tags}</div>
-            </div>
-          </Link>
-          <p className="description">{note.description}</p>
-          <div className="options">
-            <Button className='button-edit' onClick={() => { setSelectedId(note._id) }}>
-              <i className="fa-solid fa-pen-to-square"></i>
-            </Button>
-            <Button className='button-delete' onClick={() => dispatch(deleteNote(note._id))}>
-              <i className="fa-solid fa-trash"></i>
-            </Button>
-            <div className="date">
-              {moment(note.postDate).fromNow()}
+    <>
+      {
+        id === note.userId ? (
+          <div className='grid-container'>
+            <div
+              className="grid-item"
+              style={{ background: `#${note.color}` }}
+              key={note._id}
+            >
+              <div className="content-text">
+                <Link to={`/detail-note/${note._id}/${note.title}/${note.description}/${note.tags}/${note.color}/${note.postDate}`}>
+                  <div className="content-title">
+                    <h4>{note.title}</h4>
+                    <div className="right-tag" style={{ background: '#fff', color: `#${note.color}` }}>{note.tags}</div>
+                  </div>
+                </Link>
+                <p className="description">{note.description}</p>
+                <div className="options">
+                  <Button className='button-edit' onClick={() => { setSelectedId(note._id) }}>
+                    <i className="fa-solid fa-pen-to-square"></i>
+                  </Button>
+                  <Button className='button-delete' onClick={() => dispatch(deleteNote(note._id))}>
+                    <i className="fa-solid fa-trash"></i>
+                  </Button>
+                  <div className="date">
+                    {moment(note.postDate).fromNow()}
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
-    </div>
+        ) : (
+          <div style={{ display: 'none' }}></div>
+        )
+      }
+    </>
+    // <div className='grid-container'>
+    //   <div
+    //     className="grid-item"
+    //     style={{ background: `#${note.color}` }}
+    //     key={note._id}
+    //   >
+    //     <div className="content-text">
+    //       <Link to={`/detail-note/${note._id}/${note.title}/${note.description}/${note.tags}/${note.color}/${note.postDate}`}>
+    //         <div className="content-title">
+    //           <h4>{note.title}</h4>
+    //           <div className="right-tag" style={{ background: '#fff', color: `#${note.color}` }}>{note.tags}</div>
+    //         </div>
+    //       </Link>
+    //       <p className="description">{note.description}</p>
+    //       <div className="options">
+    //         <Button className='button-edit' onClick={() => { setSelectedId(note._id) }}>
+    //           <i className="fa-solid fa-pen-to-square"></i>
+    //         </Button>
+    //         <Button className='button-delete' onClick={() => dispatch(deleteNote(note._id))}>
+    //           <i className="fa-solid fa-trash"></i>
+    //         </Button>
+    //         <div className="date">
+    //           {moment(note.postDate).fromNow()}
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
   )
 }
 
